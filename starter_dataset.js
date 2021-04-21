330781338d3739a8842d6ddb8dc89ec9531e62ff
kellyf22 <73838061+kellyf22@users.noreply.github.com>
2021-04-20 20:06:26 -0400

    made coordinates decimal format, fixed syntax error

--- starter_dataset.js
+++ starter_dataset.js
@@ -5,7 +5,7 @@ var pirates = [
       atk_no: "044-21",
       atk_type: "boarded",
       vsl_type: "Product Tanker",
-      location: ["1:16.3N", "104:12.8E"],
+      location: [1.27, 104.21],
       loc_det: "Singapore Straits",
       narration: "16.04.2021: 1740 UTC: Posn: 01:16.03N – 104:12.08E, Singapore Straits. Duty Engineer on routine rounds on a tanker underway, noticed someone was trying to open the steering gear entrance door from the poop deck and immediately informed the bridge. Alarm raised and all deck and accommodation lights switched on. Hearing the alarm, the perpetrators escaped. Crew mustered and a search was carried out. Nothing reported stolen. VTIS informed",
       dt_inc: "2021-04-16" 
@@ -16,7 +16,7 @@ var pirates = [
         atk_no: "043-21",
         atk_type: "boarded",
         vsl_type: "Bulk Carrier",
-        location: [" 01:16.4N", "104:14.3E"],
+        location: [1.273, 104.23],
         loc_det: "Singapore Straits",
         narration: "16.04.2021: 2100 UTC: Posn: 01:16.4N – 104:14.3E, Singapore Straits. Four robbers boarded a bulk carrier underway. They assaulted and injured the fourth engineer, stole engine spares and escaped. Alarm raised, PA announcement made and crew mustered. On searching the vessel no robbers found. Incident reported to VTS.",
         dt_inc: "2021-04-16" 
@@ -27,7 +27,7 @@ var pirates = [
         atk_no: "042-21",
         atk_type: "boarded",
         vsl_type: "LPG Tanker",
-        location: [" 12:00.59S", "077:13.12W"],
+        location: [-12.00, -77.218],
         loc_det: "Callao Anchorage",
         narration: "14.04.2021: 0040 UTC: Posn: 12:00.59S - 077:13.12W, Callao Anchorage, Peru. Three robbers armed with knives boarded an anchored LPG tanker. Two robbers assaulted a duty AB on routine rounds. Alarm raised and crew mustered. Seeing the alerted crew, the robbers escaped with the stolen ship’s properties. Incident reported to authorities through local agent.",
         dt_inc: "2021-04-14" 
@@ -38,10 +38,10 @@ var pirates = [
         atk_no: "042-21",
         atk_type: "boarded",
         vsl_type: "Bulk Carrier",
-        location: ["01:15.5N" "104:12.04E"],
+        location: [1.258, 104.200],
         loc_det: "Singapore Straits",
         narration: "10.04.2021: 1715 UTC: Posn: 01:15.5N – 104:12.04E, Singapore Straits. Four robbers armed with knives boarded a bulk carrier underway. They broke into the engine room, stole engine spares, and escaped. The robbers were noticed by the duty crew who immediately informed the bridged and raised the alarm. Incident reported to VTIS and a patrol boat escorted the vessel until she was clear of the area. All crew reported safe.",
         dt_inc: "2021-04-10" 
   
-      },
+      }
   ];
