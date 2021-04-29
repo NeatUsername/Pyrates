9def2c9682643620bd8f4def69ecb1b5f5ed7470
Neat_Username <67394007+NeatUsername@users.noreply.github.com>
2021-04-28 21:22:09 -0400

    Flask App is working, try the only route available

--- app.py
+++ app.py
@@ -2,14 +2,15 @@
 import numpy as np
 import pymongo
 from flask import Flask, jsonify
+import json
 
 
 # Setup Mongo DB Connection
 conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
-db = client.pyrates_db
-collection = db.items
-
+db = client.pyrates_cleaned_db
+collection = db.items_cleaned
+pyrate_data = db["items_cleaned"].find()
 #################################################
 # Flask Setup
 #################################################
@@ -33,44 +34,39 @@ def welcome():
 
 ## This is SQLAlchemy Code, we'll need to adapt it to work with our Mongo DB 
 
+## Site your sources:  https://medium.com/swlh/inserting-and-reading-mongodb-documents-from-a-python-flask-api-4fa7be61e45
+
 @app.route("/api/v1.0/data")
-def data():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
-
-    session.close()
-
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-
-    return jsonify(all_names)
+def get():
+    documents = collection.find()
+    response = []
+    for document in documents:
+        document['_id'] = str(document['_id'])
+        response.append(document)
+    return json.dumps(response)
 
 
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+# @app.route("/api/v1.0/passengers")
+# def passengers():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+#     """Return a list of passenger data including the name, age, and sex of each passenger"""
+#     # Query all passengers
+#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
 
-    session.close()
+#     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+#     # Create a dictionary from the row data and append to a list of all_passengers
+#     all_passengers = []
+#     for name, age, sex in results:
+#         passenger_dict = {}
+#         passenger_dict["name"] = name
+#         passenger_dict["age"] = age
+#         passenger_dict["sex"] = sex
+#         all_passengers.append(passenger_dict)
 
-    return jsonify(all_passengers)
+#     return jsonify(all_passengers)
 
 
 if __name__ == '__main__':
